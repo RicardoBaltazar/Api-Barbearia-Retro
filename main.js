75118c1eaab9bd6d88f308c8f60e140384d76197
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-05-30 20:33:00 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-
-
 //função para chamar a animação
-document.querySelector('#submit').addEventListener('click', function(e){
-    //e.preventDefault()
-    
-    document.querySelector('.modal').style.visibility = 'visible'
-
+document.querySelector('#submit').addEventListener('click', function(){
+    if(document.querySelector('#email').value == '' || document.querySelector('#pass').value == ''){
+        alert('Dados inválidos')
+    } else {
+        document.querySelector('.modal').style.visibility = 'visible'
+        document.querySelector('#email').value = ''
+        document.querySelector('#pass').value = ''
+    }
 })
 
 //função para fechar o modal
 document.querySelector('#btnClose').addEventListener('click', function(){
     document.querySelector('.modal').style.visibility = 'hidden'
 })
+
